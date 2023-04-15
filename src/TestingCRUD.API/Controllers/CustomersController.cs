91869d1c9d1399ab8d89d626b2af12736a9042ca
Luiz Gustavo <luiz.gustavo@conferironline.com.br>
2023-04-15 15:37:37 -0300

    commit

--- src/TestingCRUD.API/Controllers/CustomersController.cs
+++ src/TestingCRUD.API/Controllers/CustomersController.cs
@@ -38,7 +38,7 @@ namespace TestingCRUD.API.Controllers
 
             return Ok(customerVM);
         }
-        //teste commit
+        //teste commit 2
         [HttpPost("CreateCustomer")]
         public async Task<IActionResult> CreateCustomer([FromBody] CreateInputModel model)
         {
