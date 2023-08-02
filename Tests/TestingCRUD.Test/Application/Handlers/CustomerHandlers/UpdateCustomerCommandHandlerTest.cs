34d73d597ab41375af2b999fcd0b8fea56d9b08a
Luiz Gustavo <luiz.gustavo@conferironline.com.br>
2023-08-02 12:23:56 -0300

    feat: add testes do create e update customers

--- Tests/TestingCRUD.Test/Application/Handlers/CustomerHandlers/UpdateCustomerCommandHandlerTest.cs
+++ Tests/TestingCRUD.Test/Application/Handlers/CustomerHandlers/UpdateCustomerCommandHandlerTest.cs
@@ -2,7 +2,6 @@
 
 public class UpdateCustomerCommandHandlerTest
 {
-    
     private Mock<ICustomerRepository> _customerRepositoryMock;
     private Mock<ICustomerReadRepository> _customerReadRepositoryMock;
     public UpdateCustomerCommandHandlerTest()
@@ -25,17 +24,17 @@ public class UpdateCustomerCommandHandlerTest
 
         var inputCustomerCommand = new UpdateCustomerCommand(customerInputModel.Cpf, customerInputModel);
 
-        _customerRepositoryMock
-            .Setup(x => x.UpdateAsync(It.IsAny<string>(), It.IsAny<Customer>(), It.IsAny<CancellationToken>()))
-            .ReturnsAsync(true);
-
-        var customer = new Customer("Jhon", "14785236912", "jhon@example.com.br", "88-5522-8874");
+        var customer = new Customer("Jhon", "14785236988", "jhon@example.com.br", "88-5522-8874");
 
         _customerReadRepositoryMock
             .Setup(x => x.GetByCpf(It.IsAny<string>(), It.IsAny<CancellationToken>()))
             .ReturnsAsync(customer);
 
-        var validator = new UpdateCustomerValidator(_customerReadRepositoryMock.Object);
+        _customerRepositoryMock
+            .Setup(x => x.UpdateAsync(It.IsAny<string>(), It.IsAny<Customer>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(true);
+
+        var validator = new UpdateCustomerValidator();
 
         var handler = new UpdateCustomerCommandHandler(_customerRepositoryMock.Object, _customerReadRepositoryMock.Object, validator);
 
@@ -47,5 +46,429 @@ public class UpdateCustomerCommandHandlerTest
         _customerRepositoryMock.Verify(x => x.UpdateAsync(It.IsAny<string>(), It.IsAny<Customer>(), It.IsAny<CancellationToken>()), Times.Once());
     }
 
+    [Fact]
+    public async Task Handle_InvalidCpf_ReturnsFalse()
+    {
+        // Arrange
+        var customerInputModel = new CustomerInputModel
+        {
+            Name = "Jhon",
+            Cpf = "14785236912",
+            Email = "jhon@example.com.br",
+            Phone = "88-5522-8874"
+        };
 
+        var inputCustomerCommand = new UpdateCustomerCommand(customerInputModel.Cpf, customerInputModel);
+
+        _customerReadRepositoryMock
+            .Setup(x => x.GetByCpf(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((Customer)null);
+
+        var validator = new UpdateCustomerValidator();
+
+        var handler = new UpdateCustomerCommandHandler(_customerRepositoryMock.Object, _customerReadRepositoryMock.Object, validator);
+
+        // Act
+        var result = await handler.Handle(inputCustomerCommand, CancellationToken.None);
+
+        // Assert
+        result.ShouldBeFalse();
+    }
+
+    [Fact]
+    public async Task Handle_ShortNameInCommand_ThrowsValidationException()
+    {
+        // Arrange
+        var customerInputModel = new CustomerInputModel
+        {
+            Name = "ze",
+            Cpf = "77788855547",
+            Email = "zeMuniz@example.com",
+            Phone = "99-99999-9999"
+        };
+
+        var updateCustomerCommand = new UpdateCustomerCommand(customerInputModel.Cpf, customerInputModel);
+
+        _customerRepositoryMock
+            .Setup(x => x.UpdateAsync(It.IsAny<string>(), It.IsAny<Customer>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(true);
+
+        var existingCustomer = new Customer("ExistingCustomer", "77788855547", "existing@example.com", "99-99999-9999");
+        _customerReadRepositoryMock
+            .Setup(x => x.GetByCpf(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(existingCustomer);
+
+        var validator = new UpdateCustomerValidator();
+
+        var handler = new UpdateCustomerCommandHandler(_customerRepositoryMock.Object, _customerReadRepositoryMock.Object, validator);
+
+        // Act & Assert
+        var exception = await Should.ThrowAsync<ValidationException>(() => handler.Handle(updateCustomerCommand, CancellationToken.None));
+        exception.Message.ShouldContain("O campo nome não pode ser menor que 02 caracteres");
+    }
+
+    [Fact]
+    public async Task Handle_InvalidEmailInCommand_ThrowsValidationException()
+    {
+        // Arrange
+        var customerInputModel = new CustomerInputModel
+        {
+            Name = "Marcele",
+            Cpf = "77788855547",
+            Email = "Marcelez@",
+            Phone = "99-99999-9999"
+        };
+
+        var updateCustomerCommand = new UpdateCustomerCommand(customerInputModel.Cpf, customerInputModel);
+
+        _customerRepositoryMock
+            .Setup(x => x.UpdateAsync(It.IsAny<string>(), It.IsAny<Customer>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(true);
+
+        _customerReadRepositoryMock
+            .Setup(x => x.GetByCpf(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((string cpf, CancellationToken cancellationToken) => null);
+
+
+        var validator = new UpdateCustomerValidator();
+
+        var handler = new UpdateCustomerCommandHandler(_customerRepositoryMock.Object, _customerReadRepositoryMock.Object, validator);
+
+        // Act & Assert
+        var exception = await Should.ThrowAsync<ValidationException>(() => handler.Handle(updateCustomerCommand, CancellationToken.None));
+        exception.Message.ShouldContain("Formato de email inválido");
+    }
+
+    [Fact]
+    public async Task Handle_InvalidFormatPhoneInCommand_ThrowsValidationException()
+    {
+        // Arrange
+        var customerInputModel = new CustomerInputModel
+        {
+            Name = "Marcele",
+            Cpf = "77788855547",
+            Email = "Marcelez@example.com",
+            Phone = "99999999999"
+        };
+
+        var updateCustomerCommand = new UpdateCustomerCommand(customerInputModel.Cpf, customerInputModel);
+
+        _customerRepositoryMock
+            .Setup(x => x.UpdateAsync(It.IsAny<string>(), It.IsAny<Customer>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(true);
+
+        _customerReadRepositoryMock
+            .Setup(x => x.GetByCpf(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((string cpf, CancellationToken cancellationToken) => null);
+
+        var validator = new UpdateCustomerValidator();
+
+        var handler = new UpdateCustomerCommandHandler(_customerRepositoryMock.Object, _customerReadRepositoryMock.Object, validator);
+
+        // Act & Assert
+        var exception = await Should.ThrowAsync<ValidationException>(() => handler.Handle(updateCustomerCommand, CancellationToken.None));
+        exception.Message.ShouldContain("Formato de telefone inválido xx-xxxxx-xxxx");
+    }
+
+    [Fact]
+    public async Task Handle_EmptyNameInCommand_ThrowsValidationException()
+    {
+        // Arrange
+        var customerInputModel = new CustomerInputModel
+        {
+            Name = "",
+            Cpf = "77788855547",
+            Email = "Marcelez@example.com",
+            Phone = "99999999999"
+        };
+
+        var updateCustomerCommand = new UpdateCustomerCommand(customerInputModel.Cpf, customerInputModel);
+
+        _customerRepositoryMock
+            .Setup(x => x.UpdateAsync(It.IsAny<string>(), It.IsAny<Customer>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(true);
+
+        _customerReadRepositoryMock
+            .Setup(x => x.GetByCpf(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((string cpf, CancellationToken cancellationToken) => null);
+
+        var validator = new UpdateCustomerValidator();
+
+        var handler = new UpdateCustomerCommandHandler(_customerRepositoryMock.Object, _customerReadRepositoryMock.Object, validator);
+
+        // Act & Assert
+        var exception = await Should.ThrowAsync<ValidationException>(() => handler.Handle(updateCustomerCommand, CancellationToken.None));
+        exception.Message.ShouldContain("Digite um nome");
+    }
+
+    [Fact]
+    public async Task Handle_EmptyCpfInCommand_ThrowsValidationException()
+    {
+        //Arrange
+        var customerInputModel = new CustomerInputModel
+        {
+            Name = "Maria",
+            Cpf = "",
+            Email = "Maria@email.com",
+            Phone = "21985786958"
+        };
+
+        var updateCustomerCommand = new UpdateCustomerCommand(customerInputModel.Cpf, customerInputModel);
+
+        _customerRepositoryMock
+            .Setup(x => x.UpdateAsync(It.IsAny<string>(), It.IsAny<Customer>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(true);
+
+        _customerReadRepositoryMock
+            .Setup(x => x.GetByCpf(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((string cpf, CancellationToken cancellationToken) => null);
+
+        var validator = new UpdateCustomerValidator();
+
+        var handler = new UpdateCustomerCommandHandler(_customerRepositoryMock.Object, _customerReadRepositoryMock.Object, validator);
+
+        //Act & Assert
+        var exception = await Should.ThrowAsync<ValidationException>(() => handler.Handle(updateCustomerCommand, CancellationToken.None));
+        exception.Message.ShouldContain("Digite o seu Cpf");
+    }
+
+    [Fact]
+    public async Task Handle_EmptyEmailInCommand_ThrowsValidationException()
+    {
+        //Arrange
+        var customerInputModel = new CustomerInputModel
+        {
+            Name = "",
+            Cpf = "12345678955",
+            Email = "",
+            Phone = "21998547899"
+        };
+
+        var updateCustomerCommand = new UpdateCustomerCommand(customerInputModel.Cpf, customerInputModel);
+
+        _customerRepositoryMock
+            .Setup(x => x.CreateAsync(It.IsAny<Customer>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((Customer customer, CancellationToken cancellationToken) =>
+            {
+                var createdCustomer = new Customer(
+                    customer.Name,
+                    customer.Cpf,
+                    customer.Email,
+                    customer.Phone);
+
+                return createdCustomer;
+            });
+
+        _customerReadRepositoryMock
+            .Setup(x => x.GetByCpf(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((string cpf, CancellationToken cancellationToken) => null);
+
+        var validator = new UpdateCustomerValidator();
+
+        var handler = new UpdateCustomerCommandHandler(_customerRepositoryMock.Object, _customerReadRepositoryMock.Object, validator);
+
+        //Acte & Assert
+        var exception = await Should.ThrowAsync<ValidationException>(() => handler.Handle(updateCustomerCommand, CancellationToken.None));
+        exception.Message.ShouldContain("Digite um Email");
+    }
+
+    [Fact]
+    public async Task Handle_EmptyPhoneInCommand_ThrowsValidationException()
+    {
+        //Arrange
+        var customerInputModel = new CustomerInputModel
+        {
+            Name = "nameTeste",
+            Cpf = "15975385246",
+            Email = "Test@example.com",
+            Phone = ""
+        };
+
+        var inputCustomerCommand = new UpdateCustomerCommand(customerInputModel.Cpf, customerInputModel);
+
+        _customerRepositoryMock
+            .Setup(x => x.CreateAsync(It.IsAny<Customer>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((Customer customer, CancellationToken cancellationToken) =>
+            {
+                var createdCustomer = new Customer(
+                    customer.Name,
+                    customer.Cpf,
+                    customer.Email,
+                    customer.Phone);
+
+                return createdCustomer;
+            });
+
+        _customerReadRepositoryMock
+            .Setup(x => x.GetByCpf(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((string cpf, CancellationToken cancellationToken) => null);
+
+        var validator = new UpdateCustomerValidator();
+
+        var handler = new UpdateCustomerCommandHandler(_customerRepositoryMock.Object, _customerReadRepositoryMock.Object, validator);
+
+        //Act e Assert
+        var exception = await Should.ThrowAsync<ValidationException>(() => handler.Handle(inputCustomerCommand, CancellationToken.None));
+        exception.Message.ShouldContain("Digite um telefone");
+    }
+
+    [Fact]
+    public async Task Handle_LongNameInCommand_ThrowsValidationException()
+    {
+        //Arrange
+        var customerInputModel = new CustomerInputModel
+        {
+            Name = "marcele Gonçalves munis de souza de garcia",
+            Cpf = "77788855547",
+            Email = "Marcelez@example.com",
+            Phone = "99999999999"
+        };
+
+        var inputCustomerCommand = new UpdateCustomerCommand(customerInputModel.Cpf, customerInputModel);
+
+        _customerRepositoryMock
+            .Setup(x => x.CreateAsync(It.IsAny<Customer>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((Customer customer, CancellationToken cancellationToken) =>
+            {
+                var createdCustomer = new Customer(
+                    customer.Name,
+                    customer.Cpf,
+                    customer.Email,
+                    customer.Phone);
+
+                return createdCustomer;
+            });
+
+        _customerReadRepositoryMock
+            .Setup(x => x.GetByCpf(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((string cpf, CancellationToken cancellationToken) => null);
+
+        var validator = new UpdateCustomerValidator();
+
+        var handler = new UpdateCustomerCommandHandler(_customerRepositoryMock.Object, _customerReadRepositoryMock.Object, validator);
+
+        //Act e Assert
+        var exception = await Should.ThrowAsync<ValidationException>(() => handler.Handle(inputCustomerCommand, CancellationToken.None));
+        exception.Message.ShouldContain("O campo nome não pode passar de 30 caracteres");
+    }
+
+    [Fact]
+    public async Task Handle_LongCpfInCommand_ThrowsValidationException()
+    {
+        //Arrange
+        var customerInputModel = new CustomerInputModel
+        {
+            Name = "Marcos",
+            Cpf = "123456789456123",
+            Email = "marcos@example.com",
+            Phone = "21589645687"
+        };
+
+        var updateCustomerCommand = new UpdateCustomerCommand(customerInputModel.Cpf, customerInputModel);
+
+        _customerRepositoryMock
+            .Setup(x => x.CreateAsync(It.IsAny<Customer>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((Customer customer, CancellationToken cancellationToken) =>
+            {
+                var createdCustomer = new Customer(
+                    customer.Name,
+                    customer.Cpf,
+                    customer.Email,
+                    customer.Phone);
+
+                return createdCustomer;
+            });
+
+        _customerReadRepositoryMock
+            .Setup(x => x.GetByCpf(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((string cpf, CancellationToken cancellationToken) => null);
+
+        var validator = new UpdateCustomerValidator();
+
+        var handler = new UpdateCustomerCommandHandler(_customerRepositoryMock.Object, _customerReadRepositoryMock.Object, validator);
+
+        //Act & Assert
+        var exception = await Should.ThrowAsync<ValidationException>(() => handler.Handle(updateCustomerCommand, CancellationToken.None));
+        exception.Message.ShouldContain("O campo Cpf não pode passar de 11 caracteres");
+    }
+
+    [Fact]
+    public async Task Handle_LongPhoneInCommand_ThrowsValidationException()
+    {
+        //Arrange
+        var customerInputModel = new CustomerInputModel
+        {
+            Name = "Carla",
+            Cpf = "12345678991",
+            Email = "carlinha@example.com",
+            Phone = "12345678903216549870"
+        };
+
+        var updateCustomerCommand = new UpdateCustomerCommand(customerInputModel.Cpf, customerInputModel);
+
+        _customerRepositoryMock
+            .Setup(x => x.CreateAsync(It.IsAny<Customer>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((Customer customer, CancellationToken cancellationToken) =>
+            {
+                var createdCustomer = new Customer(
+                    customer.Name,
+                    customer.Cpf,
+                    customer.Email,
+                    customer.Phone);
+
+                return createdCustomer;
+            });
+
+        _customerReadRepositoryMock
+            .Setup(x => x.GetByCpf(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((string cpf, CancellationToken cancellationToken) => null);
+
+        var validator = new UpdateCustomerValidator();
+
+        var handler = new UpdateCustomerCommandHandler(_customerRepositoryMock.Object, _customerReadRepositoryMock.Object, validator);
+
+        //Act e Assert
+        var exception = await Should.ThrowAsync<ValidationException>(() => handler.Handle(updateCustomerCommand, CancellationToken.None));
+        exception.Message.ShouldContain("Telefone não pode ter mais de 11 dígitos contando com DDD");
+    }
+
+    [Fact]
+    public async Task Handle_InvalidCpfInCommand_ThrowsValidationExcetion()
+    {
+        //Arrange
+        var customerInputModel = new CustomerInputModel
+        {
+            Name = "Marcela",
+            Cpf = "777.888.555-66",
+            Email = "Marcelez@example.com",
+            Phone = "99999999999"
+        };
+
+        var inputCustomerCommand = new UpdateCustomerCommand(customerInputModel.Cpf, customerInputModel);
+
+        _customerRepositoryMock
+            .Setup(x => x.CreateAsync(It.IsAny<Customer>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((Customer customer, CancellationToken cancellationToken) =>
+            {
+                var createdCustomer = new Customer(
+                    customer.Name,
+                    customer.Cpf,
+                    customer.Email,
+                    customer.Phone);
+
+                return createdCustomer;
+            });
+
+        _customerReadRepositoryMock
+            .Setup(x => x.GetByCpf(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((string cpf, CancellationToken cancellationToken) => null);
+
+        var validator = new UpdateCustomerValidator();
+
+        var handler = new UpdateCustomerCommandHandler(_customerRepositoryMock.Object, _customerReadRepositoryMock.Object, validator);
+
+        //Act e Assert
+        var exception = await Should.ThrowAsync<ValidationException>(() => handler.Handle(inputCustomerCommand, CancellationToken.None));
+        exception.Message.ShouldContain("Cpf inválido com pontos,traços ou letras");
+    }
 }
--- src/TestingCRUD.Aplication/Validations/CustomerCommandValidation/CreateCustomerValidator.cs
+++ src/TestingCRUD.Aplication/Validations/CustomerCommandValidation/CreateCustomerValidator.cs
@@ -1,36 +1,18 @@
 ﻿using FluentValidation;
 
 using TestingCRUD.Domain.Repositories;
-using TestingCRUD.Application.InputModels;
 
 namespace TestingCRUD.Application.Validations.CustomerCommandValidation;
-public class CreateCustomerValidator : AbstractValidator<CustomerInputModel>
+public class CreateCustomerValidator : UpdateCustomerValidator
 {
-    private readonly ICustomerReadRepository? _customerReadRepository;
+    private readonly ICustomerReadRepository _customerReadRepository;
+
     public CreateCustomerValidator(ICustomerReadRepository customerReadRepository)
     {
         _customerReadRepository = customerReadRepository;
 
-        RuleFor(c => c.Name)
-           .NotEmpty().WithMessage("Digite um nome")
-           .MaximumLength(30).WithMessage("O campo nome não pode passar de 30 caracteres")
-           .MinimumLength(3).WithMessage("O campo nome não pode ser menor que 02 caracteres");
-
-
         RuleFor(c => c.Cpf)
-            .NotEmpty().WithMessage("Digite o seu Cpf")
-            .MaximumLength(11).WithMessage("O campo Cpf não pode passar de 11 caracteres")
-            .Matches("[0-9]{11}").WithMessage("Cpf inválido com pontos,traços ou letras")
             .MustAsync(CpfIsValid).WithMessage("Este Cpf já está cadastrado");
-
-        RuleFor(c => c.Email)
-            .NotEmpty().WithMessage("Digite um email")
-            .EmailAddress().WithMessage("Formato de email inválido");
-
-        RuleFor(c => c.Phone)
-            .NotEmpty().WithMessage("Digite um telefone")
-            .Matches("^([1-9]{2})-(?:[2-8]|9[1-9])[0-9]{3}-[0-9]{4}$").WithMessage("Formato de telefone inválido xx-xxxxx-xxxx")
-            .MaximumLength(14).WithMessage("Telefone não pode ter mais de 11 dígitos contando com DDD");
     }
 
     private async Task<bool> CpfIsValid(string cpf, CancellationToken cancellationToken)
